e6b3f2b3086726213d08063427aed676fe00f57c
Kevin <kevhines@gmail.com>
2021-02-26 15:39:36 -0500

    commit tree 49df257af5de35e6d211e8f40c060a765720ae38

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,28 @@
 class ArtistsController < ApplicationController
+    get '/artists' do
+        @artists = Artist.all
+        erb :'/artists/index'
+    end
+    
+    get '/artists/:slug' do
+        @artist = Artist.find_by_slug(params[:slug])
+        erb :'/artists/show'
+    end
+
+    get '/artists/:slug/edit' do
+        @artist = Artist.find_by_slug(params[:slug])
+        erb :'/artists/edit'
+    end
+    
+    patch '/artists/:slug' do
+         @artist = Artist.find_by_slug(params[:slug])
+         @artist.update(params[:artist])
+        # #@artist.artist = Artist.find_or_create_by(name: params[:artist][:name])
+        # @artist.genre_ids = params[:genres]
+        # @artist.save
+    
+     #   flash[:message] = "Successfully updated song."
+        redirect("/artists/#{@artist.slug}")
+    end
+    
 end
